fe2ad7a6d162ce32543fbad2156a9c7dc0ff825e
VicYing <rainying153@gmail.com>
2016-11-23 00:39:04 +0800

    init
    init

--- install/ init.sh
+++ install/ init.sh
@@ -1,14 +1,8 @@
-yum  -y install epel-release
-
-yum -y install git 
-
 yum -y install python-pip
 
 pip install --upgrade pip
 
-git ~/docker_install 
-
-cp ~/docker_install  /etc/yum.repos.d/docker.repo
+cp ~/git/docker_centos/file/docker.repo  /etc/yum.repos.d/docker.repo
 
 yum -y install docker-engine
 
@@ -17,4 +11,5 @@ pip install docker-compose
 pip install backports.ssl_match_hostname --upgrade
 
 systemctl start docker.service
+
 systemctl enable docker.service
