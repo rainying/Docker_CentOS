0ceca29ffe803128a7efaa94e6d7939b86ba4b5b
Vic Ying <rainying153@gmail.com>
2016-12-07 18:28:00 +0800

    1.0.1 Mysql install

---
+++ container/mysql.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+###
+# 請以root身份登入
+##
+
+## docker run --name [mysql container name(mysql)] -e MYSQL_ROOT_PASSWORD=[mysql password(test@password)] -d mysql:[mysql tag(9)]
+
+docker run --name mysql -e MYSQL_ROOT_PASSWORD=test@password -p 3306:3306 -d mysql:9
