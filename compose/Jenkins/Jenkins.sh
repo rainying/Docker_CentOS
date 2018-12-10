15c10d62065c793d3acda6b3cfe57683aa064227
vicying <windincns@gmil.com>
2018-12-10 16:49:17 +0800

    修正Jenkins錯誤

--- compose/Jenkins/Jenkins.sh
+++ compose/Jenkins/Jenkins.sh
@@ -4,6 +4,8 @@ rm -rf /root/Jenkins
 
 /bin/cp -R ~/github/docker_centos/compose/Jenkins/file /root/Jenkins
 
+cd /root/Jenkins
+
 --原始檔 https://raw.githubusercontent.com/bitnami/bitnami-docker-jenkins/master/docker-compose.yml
 
 docker-compose down
