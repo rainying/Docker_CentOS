27655006ce52ca273c2e94e03f5127c8dda8d755
vicying <windincns@gmil.com>
2018-12-10 17:15:26 +0800

    no message

--- compose/Jenkins/Jenkins.sh
+++ compose/Jenkins/Jenkins.sh
@@ -6,9 +6,9 @@ mkdir /Docker/Jenkins/Volumes
 
 rm -rf /Docker/Jenkins
 
-/bin/cp -R ~/github/docker_centos/compose/Jenkins/Compose /root/Jenkins/Main
+/bin/cp -R ~/github/docker_centos/compose/Jenkins/Compose /Docker/Jenkins/Main
 
-cd /root/Jenkins/Main
+cd /Docker/Jenkins/Main
 
 --原始檔 https://raw.githubusercontent.com/bitnami/bitnami-docker-jenkins/master/docker-compose.yml
 
