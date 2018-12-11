989eec544a96184181b7bbc95da07915d636c1b3
vicying <windincns@gmil.com>
2018-12-11 14:37:53 +0800

    no message

--- compose/Jfrog/RapidDeploy.sh
+++ compose/Jfrog/RapidDeploy.sh
@@ -2,6 +2,7 @@ rm -rf ~/github/docker_centos
 
 git clone https://github.com/rainying/Docker_CentOS.git ~/github/docker_centos
 
-chmod +x ~/github/docker_centos/compose/Jenkins/*.sh
+chmod +x ~/github/docker_centos/compose/Jfrog/*.sh
 
-sh ~/github/docker_centos/compose/Jenkins/Jenkins.sh
+sh ~/github/docker_centos/compose/Jfrog/Jfrog.sh
+
