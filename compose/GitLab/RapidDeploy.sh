3258cf902bb8f78fa666a7117f4eae94eee745e3
vicying <windincns@gmil.com>
2018-12-11 12:01:29 +0800

    no message

--- compose/GitLab/RapidDeploy.sh
+++ compose/GitLab/RapidDeploy.sh
@@ -2,6 +2,6 @@ rm -rf ~/github/docker_centos
 
 git clone https://github.com/rainying/Docker_CentOS.git ~/github/docker_centos
 
-chmod +x ~/github/docker_centos/compose/Jenkins/*.sh
+chmod +x ~/github/docker_centos/compose/GitLab/*.sh
 
-sh ~/github/docker_centos/compose/Jenkins/Jenkins.sh
+sh ~/github/docker_centos/compose/GitLab/GitLab.sh
