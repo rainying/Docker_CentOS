d598e836925eacb0481d435f95822d641ae0719e
vicying <windincns@gmil.com>
2018-12-11 11:15:19 +0800

    no message

---
+++ compose/Jenkins/RapidDeploy.sh
@@ -0,0 +1,11 @@
+yum -y install epel-release
+
+yum -y install git 
+
+rm -rf ~/github/docker_centos
+
+git clone https://github.com/rainying/Docker_CentOS.git ~/github/docker_centos
+
+chmod +x ~/github/docker_centos/compose/Jenkins/*.sh
+
+sh ~/github/docker_centos/compose/Jenkins/Jenkins.sh
