8660f5d93e71a4ad84d9ab79d2771cfbb46093c2
vicying <windincns@gmil.com>
2018-12-19 13:21:08 +0800

    no message

--- install/init.sh
+++ install/init.sh
@@ -15,10 +15,16 @@ pip install docker-compose
 
 pip install backports.ssl_match_hostname --upgrade
 
-service docker stop
+yum -y install bridge-utils
 
+systemctl stop docker.service
 
+echo '{"bip": "192.168.210.1/24"}' > /etc/docker/daemon.json
+
+systemctl restart network
 
 systemctl start docker.service
 
 systemctl enable docker.service
+
+reboot
