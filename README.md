a75e0026ba469f7ba807eaa67e7bfd914d8959aa
vicying <windincns@gmil.com>
2018-12-21 11:51:55 +0800

    更新docker網路設定

--- README.md
+++ README.md
@@ -24,9 +24,13 @@ git clone https://github.com/rainying/Docker_CentOS.git ~/github/docker_centos
 chmod +x ~/github/docker_centos/install/*.sh
 ```
 
+以下二選一
+
 ```
-sh ~/github/docker_centos/install/init.sh
+sh ~/github/docker_centos/install/init.sh #網路需要更改
 ```
 
-
+```
+sh ~/github/docker_centos/install/init_notchangrenetwork.sh #網路無須更改
+```
 
---
+++ install/init_notchangrenetwork.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+###
+# 請以root身份登入
+##
+
+yum -y install python-pip
+
+pip install --upgrade pip
+
+cp ~/github/docker_centos/file/docker.repo  /etc/yum.repos.d/docker.repo
+
+yum -y install docker-engine
+
+pip install docker-compose
+
+pip install backports.ssl_match_hostname --upgrade
+
+#cp ~/github/docker_centos/file/daemon.json  /etc/docker/daemon.json
+
+systemctl stop docker.service
+
+systemctl start docker.service
+
+systemctl enable docker.service
+
