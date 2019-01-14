27c9f671366e1e7a304d119678c2e09636e878c7
VicYing <rainying153@gmil.com>
2019-01-14 13:44:45 +0800

    Compose_Redmind 修正

--- compose/Redmine/Redmine.sh
+++ compose/Redmine/Redmine.sh
@@ -2,9 +2,15 @@ mkdir /Docker
 
 mkdir /Docker/Redmine 
 
+mkdir /Docker/Redmine_Postgresql/
+
 mkdir /Docker/Redmine/Volumes
 
+mkdir /Docker/Redmine_Postgresql/Volumes
+
 chmod -R 777 /Docker/Redmine/Volumes
+
+chmod -R 777 /Docker/Redmine_Postgresql/Volumes
 
 rm -rf /Docker/Redmine/Main
 
